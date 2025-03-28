f0b930ae2f21dedf26f6bb9500fb2d1639165b14
ADarko22 <a.elessar22@gmail.com>
2025-03-28 20:57:51 +0100

    Improve testing of BitbucketApiClient

--- src/test/kotlin/io/github/adarko22/bitbucket/BitbucketApiClientTest.kt
+++ src/test/kotlin/io/github/adarko22/bitbucket/BitbucketApiClientTest.kt
@@ -4,7 +4,6 @@ import okhttp3.mockwebserver.MockResponse
 import okhttp3.mockwebserver.MockWebServer
 import org.junit.jupiter.api.AfterEach
 import org.junit.jupiter.api.Assertions.assertEquals
-import org.junit.jupiter.api.Assertions.assertNotNull
 import org.junit.jupiter.api.BeforeEach
 import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
@@ -19,7 +18,7 @@ class BitbucketApiClientTest {
     fun setUp() {
         mockWebServer = MockWebServer()
         mockWebServer.start()
-        val baseUrl = mockWebServer.url("/baseUrl").toString()
+        val baseUrl = mockWebServer.url("/").toString().removeSuffix("/")
         bitbucketApiClient = BitbucketApiClient(baseUrl, "username", "password")
     }
 
@@ -29,63 +28,86 @@ class BitbucketApiClientTest {
     }
 
     @Test
-    fun `getProjectKeys should return list of project keys`() {
+    fun `should detect project keys in Maven project`() {
         val mockResponse = """
             {
                 "size": 1,
                 "limit": 25,
                 "isLastPage": true,
-                "values": [
-                    {"key": "PROJ1"}
-                ],
+                "values": [{"key": "PROJ1"}],
                 "start": 0
             }
         """.trimIndent()
 
-        mockWebServer.enqueue(MockResponse().setBody(mockResponse))
-
-        val projectKeys = bitbucketApiClient.getProjectKeys()
-
-        verifyRequestPath("/baseUrl/rest/api/1.0/projects?start=0")
-        verifyResponse(projectKeys, listOf("PROJ1"))
+        testApiCall(
+            "/rest/api/1.0/projects?start=0",
+            mockResponse,
+            { bitbucketApiClient.getProjectKeys() },
+            listOf("PROJ1")
+        )
     }
 
     @Test
-    fun `getReposLinksForProjectKey should return list of repository clone URLs`() {
+    fun `should detect repository clone URLs for project key`() {
         val mockResponse = """
             {
                 "size": 1,
                 "limit": 25,
                 "isLastPage": true,
-                "values": [
-                    {
-                        "links": {
-                            "clone": [
-                                {"href": "http://example.com/scm/proj1/repo1.git", "name": "http"}
-                            ]
-                        }
+                "values": [{
+                    "links": {
+                        "clone": [{"href": "http://example.com/scm/proj1/repo1.git", "name": "http"}]
                     }
-                ],
+                }],
                 "start": 0
             }
         """.trimIndent()
 
+        testApiCall(
+            "/rest/api/1.0/projects/PROJ1/repos?start=0",
+            mockResponse,
+            { bitbucketApiClient.getReposLinksForProjectKey("PROJ1") },
+            listOf("http://example.com/scm/proj1/repo1.git")
+        )
+    }
+
+    @Test
+    fun `should return null when 'clone' array is empty`() {
+        val mockResponse = """
+            {
+                "size": 1,
+                "limit": 25,
+                "isLastPage": true,
+                "values": [{
+                    "links": {
+                        "clone": []
+                    }
+                }],
+                "start": 0
+            }
+        """.trimIndent()
+
+        testApiCall(
+            "/rest/api/1.0/projects/PROJ1/repos?start=0",
+            mockResponse,
+            { bitbucketApiClient.getReposLinksForProjectKey("PROJ1") },
+            emptyList()
+        )
+    }
+
+    private fun testApiCall(
+        expectedPath: String,
+        mockResponse: String,
+        apiCall: () -> List<String>,
+        expectedResponse: List<String>
+    ) {
         mockWebServer.enqueue(MockResponse().setBody(mockResponse))
 
-        val repoLinks = bitbucketApiClient.getReposLinksForProjectKey("PROJ1")
+        val response = apiCall()
 
-        verifyRequestPath("/baseUrl/rest/api/1.0/projects/PROJ1/repos?start=0")
-        verifyResponse(repoLinks, listOf("http://example.com/scm/proj1/repo1.git"))
-    }
-
-    private fun verifyRequestPath(expectedPath: String) {
         val recordedRequest = mockWebServer.takeRequest()
         assertEquals(expectedPath, recordedRequest.path)
-    }
 
-    private fun <T> verifyResponse(actual: List<T>, expected: List<T>) {
-        assertNotNull(actual)
-        assertEquals(expected.size, actual.size)
-        assertEquals(expected, actual)
+        assertEquals(expectedResponse, response)
     }
 }
